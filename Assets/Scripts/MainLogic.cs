48d9a7b03b8fb865652eca4693ace6d083474dc8
Rckdrigo <rckdigomed@gmail.com>
2015-01-06 12:52:25 -0600

    Adios Vuforia

--- Assets/Scripts/MainLogic.cs
+++ Assets/Scripts/MainLogic.cs
@@ -10,9 +10,9 @@ public class MainLogic : Singleton<MainLogic> {
 	public GameObject loading;
 
 	void Start(){
-		bool focusModeSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-		if (!focusModeSet) 
-			Debug.Log("Failed to set focus mode (unsupported mode).");
+		//bool focusModeSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		//if (!focusModeSet) 
+		//	Debug.Log("Failed to set focus mode (unsupported mode).");
 	}
 
 	// Use this for initialization
---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WebCam : MonoBehaviour {
+	WebCamTexture webCamTexture;
+
+	// Use this for initialization
+	void Start () {
+		webCamTexture = new WebCamTexture();
+		webCamTexture.Play();
+		GetComponent<RawImage>().texture = webCamTexture;
+		//GetComponent<RectTransform>().sizeDelta = new Vector2(,);
+	}
+}
