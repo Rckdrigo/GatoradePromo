e6d4f87267b301766647f2d55b7731ebf35e8da8
Rodrigo Medina <rckdrigomed@gmail.com>
2015-01-14 09:49:17 -0600

    Aspect script

---
+++ Assets/AspectAdapt.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public enum IosDevice{IPad,IPhone4,IPhone5}
+
+public class AspectAdapt : MonoBehaviour {
+
+	[System.Serializable]
+	public struct AspectRect{
+		public Vector3 position;
+		public Vector2 deltaSize;
+	}
+	
+	public AspectRect[] rect;
+
+	IosDevice device;
+
+	void Start(){
+	#if UNITY_IOS
+		switch(iPhone.generation){
+		case iPhoneGeneration.iPhone4:
+		device = IosDevice.IPhone4;
+			break;
+			
+			
+		case iPhoneGeneration.iPhone4S:	
+		device = IosDevice.IPhone4;
+			break;
+			
+			
+		case iPhoneGeneration.iPhone5:
+		device = IosDevice.IPhone5;
+			break;
+			
+			
+		case iPhoneGeneration.iPhone5C:
+		device = IosDevice.IPhone5;
+			break;
+			
+			
+		case iPhoneGeneration.iPhone5S:
+			device = IosDevice.IPhone5;
+			break;
+			
+		case iPhoneGeneration.iPad1Gen:
+			device = IosDevice.IPad;
+			break;
+			
+		case iPhoneGeneration.iPad2Gen:
+			device = IosDevice.IPad;
+			break;
+			
+		case iPhoneGeneration.iPad3Gen:
+			device = IosDevice.IPad;
+			break;
+			
+		case iPhoneGeneration.iPad4Gen:
+			device = IosDevice.IPad;
+			break;
+			
+		case iPhoneGeneration.iPad5Gen:
+			device = IosDevice.IPad;
+			break;
+			
+		case iPhoneGeneration.iPadAir2:
+			device = IosDevice.IPad;
+			break;
+			
+		case iPhoneGeneration.iPadMini1Gen:
+			device = IosDevice.IPad;
+			break;
+			
+		case iPhoneGeneration.iPadMini2Gen:
+			device = IosDevice.IPad;
+			break;
+		
+		default:
+			device = IosDevice.IPhone4;
+			break;
+		
+		}
+		
+		switch(device){
+		case IosDevice.IPhone4:
+			GetComponent<RectTransform>().position = rect[2].position;
+			GetComponent<RectTransform>().sizeDelta = rect[2].deltaSize;
+			break;
+			
+		case IosDevice.IPhone5:
+			GetComponent<RectTransform>().position = rect[1].position;
+			GetComponent<RectTransform>().sizeDelta = rect[1].deltaSize;
+			break;
+			
+		case IosDevice.IPad:
+			GetComponent<RectTransform>().position = rect[0].position;
+			GetComponent<RectTransform>().sizeDelta = rect[0].deltaSize;
+			break;
+		}
+    #endif
+	}
+
+}
